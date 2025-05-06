bf451f3c7b9caff1ed0affd7b5a991bdd0554996
a38603 <anhmanhc2k50@gmail.com>
2025-05-06 11:47:00 +0700

    The first commit

---
+++ Models/AccountModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Models.Framework;
+
+namespace Models
+{
+    public class AccountModel
+    {
+        private OnlineShopDbContext context = null;
+        public AccountModel()
+        {
+            context = new OnlineShopDbContext();
+        }
+        public bool Login(string username, string password)
+        {
+            object[] sqlParams =
+            {
+                new SqlParameter("@UserName", username),
+                new SqlParameter("@PassWord", password)
+            };
+            var res = context.Database.SqlQuery<bool>("EXEC Sp_Account_login @UserName, @PassWord",sqlParams).SingleOrDefault();
+            return res;
+        }
+    }
+}
---
+++ OnlineShop/Areas/Admin/AdminAreaRegistration.cs
@@ -0,0 +1,26 @@
+﻿using System.Web.Mvc;
+
+namespace OnlineShop.Areas.Admin
+{
+    public class AdminAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Admin";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+            name: "Admin_default",
+            url: "Admin/{controller}/{action}/{id}",
+            defaults: new { action = "Index",Controller="Home", id = UrlParameter.Optional },
+            namespaces: new[] { "OnlineShop.Areas.Admin.Controllers" }
+            );
+
+        }
+    }
+}
