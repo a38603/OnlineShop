04acc0fe2ee35ffe6bfb54ab9aac96fdfefe0e50
a38603 <anhmanhc2k50@gmail.com>
2025-05-07 11:44:54 +0700

    Add .gitignore and remove unnecessary files

---
+++ Models/Framework/CategoryModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.Framework
+{
+    public class CategoryModel
+    {
+        private OnlineShopDbContext context=null;
+        public CategoryModel()
+        {
+            context = new OnlineShopDbContext();
+        }
+        public List<Category> ListAll()
+        {
+            var list= context.Database.SqlQuery<Category>("Sp_Category_ListAll").ToList();
+            return list;
+        }
+    }
+}
